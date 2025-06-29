ead0922cd1a8aedb4af08bbacfab92157f86faaf
TN23VN <tronghv9abg@gmail.com>
2025-06-30 00:55:28 +0700

    commit
    create animation walk and follow camera

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -3,6 +3,7 @@ using UnityEngine;
 public class PlayerController : MonoBehaviour
 {
     [SerializeField] private Rigidbody2D rb;
+    [SerializeField] private Animator animator;
     [SerializeField] private float moveSpeed;
     public Vector3 playerMoveDirection;
 
@@ -11,11 +12,22 @@ public class PlayerController : MonoBehaviour
     {
         float inputX = Input.GetAxisRaw("Horizontal");
         float inputY = Input.GetAxisRaw("Vertical");
-        playerMoveDirection = new Vector2(inputX, inputY).normalized;
+        playerMoveDirection = new Vector3(inputX, inputY).normalized;
+
+        animator.SetFloat("MoveX", inputX);
+        animator.SetFloat("MoveY", inputY);
+        if (playerMoveDirection == Vector3.zero)
+        {
+            animator.SetBool("ismove", false);
+        }
+        else
+        {
+            animator.SetBool("ismove", true);
+        }
     }
 
     private void FixedUpdate()
     {
-        rb.linearVelocity = new Vector2(playerMoveDirection.x * moveSpeed, playerMoveDirection.y * moveSpeed);
+        rb.linearVelocity = new Vector3(playerMoveDirection.x * moveSpeed, playerMoveDirection.y * moveSpeed);
     }
 }
